549f4b07c26a0172bfc541dcc148a17bc2f121e7
MoisesLopezp <moicano324@hotmail.com>
2018-07-05 12:11:56 -0500

    Canvas loco
    asdasd

---
+++ Assets/Cosas Moy/scr_CntrlSession.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_CntrlSession : MonoBehaviour {
+
+    public static scr_CntrlSession CS;
+
+    public Text T_Respuesta;
+    public GameObject[] Preguntas = new GameObject[3];
+    public GameObject SE_session;
+
+    private void Awake()
+    {
+        CS = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
