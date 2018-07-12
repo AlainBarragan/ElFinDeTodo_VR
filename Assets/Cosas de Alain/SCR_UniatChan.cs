d3c0a2a34de495eaefdb48958d96b8205526c779
Alain <barraganalain55@gmail.com>
2018-07-12 17:00:27 -0500

    El mejor idioma del mundo y script de uniat chan

---
+++ Assets/Cosas de Alain/SCR_UniatChan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_UniatChan : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.SetLayerWeight(1, SCR_DemonLevel.malboneco);
+    }
+}
