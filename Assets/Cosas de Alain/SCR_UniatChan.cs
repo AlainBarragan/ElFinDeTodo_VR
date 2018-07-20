e369c543d6b33d5826bd32a6f9db30e0b16fc6ea
Alain <barraganalain55@gmail.com>
2018-07-19 20:28:16 -0500

    arreglada la loca esta

--- Assets/Cosas de Alain/SCR_UniatChan.cs
+++ Assets/Cosas de Alain/SCR_UniatChan.cs
@@ -11,7 +11,7 @@ public class SCR_UniatChan : MonoBehaviour
 
     private void Update()
     {
-        anim.SetLayerWeight(1, SCR_DemonLevel.malboneco);
+        anim.SetLayerWeight(1, SCR_DemonLevel.malboneco * 0.01f);
         anim.SetBool("flotar", SCR_DemonLevel.malboneco > 80);
     }
 
